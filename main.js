1b49962ec7c5b1a7f5db7d0a25321ed28d176a60
nabreu <n.abreu.s22@gmail.com>
2022-06-08 10:25:59 +0200

    wip

--- main.js
+++ main.js
@@ -1,7 +1,27 @@
 function onScroll() {
+  var element = document.getElementById('navigation')
   if (scrollY > 0) {
-    navigation.classList.add('scroll')
+    element.classList.add('scroll')
   } else {
-    navigation.classList.remove('scroll')
+    element.classList.remove('scroll')
   }
 }
+
+function openMenu() {
+  document.body.classList.add('menu-expanded')
+}
+
+function closeMenu() {
+  document.body.classList.remove('menu-expanded')
+}
+
+ScrollReveal({
+  origin: 'top',
+  distance: '30px',
+  duration: 700
+}).reveal(`
+#home,
+#home img,
+#home .stats,
+
+`)
